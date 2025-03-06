d44c97114e082e8319d6e4fb0301e28750e64d96
LOTSWA <55159891+LOTSWA@users.noreply.github.com>
2025-03-05 22:56:25 -0600

    Update Menu.java

--- MiddleEarthMadness/src/middleearthmadness/home/Menu.java
+++ MiddleEarthMadness/src/middleearthmadness/home/Menu.java
@@ -1,7 +1,21 @@
 package middleearthmadness.home;
 import java.util.*;
 
-
+public class Menu{
+	 public static void mainMenu(){
+		System.out.println("What Would you like to do \n");
+		System.out.println("1 Add new Character\n");
+		System.out.println("2 View all Characters\n");
+		System.out.println("3 Upade a character\n");
+		System.out.println("4 Delete a character\n");
+		System.out.println("5 Execute all characters' attack actions\n");
+		System.out.println("6 Exit\n");
+		
+		Scanner myobj = new Scanner(System.in);
+		String input = myobj.nextLine();
+		System.out.println("option" + input);
+	}
+}
 
 
 
