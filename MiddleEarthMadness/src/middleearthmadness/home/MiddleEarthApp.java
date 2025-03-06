eb14cfb592f35ffbad68f1fd4b2f7344c7f6db03
LOTSWA <55159891+LOTSWA@users.noreply.github.com>
2025-03-05 21:05:36 -0600

    Character stuff

--- MiddleEarthMadness/src/middleearthmadness/home/MiddleEarthApp.java
+++ MiddleEarthMadness/src/middleearthmadness/home/MiddleEarthApp.java
@@ -1,16 +1,21 @@
 package middleearthmadness.home;
 
 import middlearthmadnesss.info.CharacterManager;
+import middlearthmadnesss.info.Elf;
 import middlearthmadnesss.info.MiddleEarthCharacter;
+
 
 public class MiddleEarthApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			CharacterManager.addCharacter();
-			CharacterManager.getCharacter();
-			CharacterManager.updateCharacter();
-			CharacterManager.deleteCharacter();
+		
+		Elf myElf = new Elf("Legolas", 5.0, 6.0);
+			
+			myElf.getCharacter();
+			myElf.updateCharacter(myElf,"Galadriel",10.0,14.0);
+			myElf.deleteCharacter();
+			myElf.displayInfo();
 			
 	}
 
