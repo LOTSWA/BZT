f8ca5b52a99e4c3e71c05f4fcdc15f18b9463ee9
Stattius <xxblade6475xx@gmail.com>
2025-03-05 18:51:43 -0600

    Started CharacterManager, first boolean implementation

--- MiddleEarthMadness/src/middlearthmadnesss/info/CharacterManager.java
+++ MiddleEarthMadness/src/middlearthmadnesss/info/CharacterManager.java
@@ -1,5 +1,22 @@
 package middlearthmadnesss.info;
 
 public class CharacterManager {
-
+	
+	public MiddleEarthCharacter[] characters;
+	public int size;
+	
+	public CharacterManager()
+	{
+		characters = new MiddleEarthCharacter[1];
+		size = 0;
+	}
+	
+	public boolean addCharacter(MiddleEarthCharacter c)
+	{
+		if (size == characters.length)
+		{
+			MiddleEarthCharacter[] temp = new MiddleEarthCharacter[characters.length * 2];
+			
+		}
+	}
 }
